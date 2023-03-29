1a54e05678132a69537e5348b3093b8fd8bd4dc6
pkrawc <patrick@dreadful.design>
2023-03-29 14:59:30 -0500

    update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,19 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata = {
   title: "Tints & Hints",
   description: "Multiplayer Color Guessing Game",
+  openGraph: {
+    title: "Tints & Hints",
+    description: "Multiplayer Color Guessing Game",
+    url: "https://tints-and-hints.vercel.app/",
+    siteName: "Tints & Hints",
+    images: [
+      {
+        url: "https://tints-and-hints.vercel.app/og-image.png",
+        width: 800,
+        height: 600,
+      },
+    ],
+  },
 }
 
 export default function RootLayout({
